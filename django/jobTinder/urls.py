53807d49f92f81248854187c1c3cd96355047483
Marcos Antonio Carneiro de Paula <cardepaula@gmail.com>
2019-04-11 09:07:04 -0300

    feat(template): Teste de template

--- django/jobTinder/urls.py
+++ django/jobTinder/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from .views import views
 
+#urlpatterns = [
+#    path('', views.candidatos_list, name='candidatos_list'),
+#]
+
+##
+# from django.conf.urls import patterns, include, url
+
+#urlpatterns = patterns('',
+#    url(r'', "views.home"),
+#)
+
 urlpatterns = [
-    path('', views.candidatos_list, name='candidatos_list'),
+    path('', views.home, name='home'),
 ]
--- django/jobTinder/views/views.py
+++ django/jobTinder/views/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 
-def candidatos_list(request):
-    return render(request, 'jobTinder/candidatos_list.html', {})
+#def candidatos_list(request):
+#    return render(request, 'jobTinder/candidatos_list.html', {})
+
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+import os
+import random
+
+def home(request):
+  directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../static/tinder/profiles")
+  filenames = ["tinder/profiles/%s" % fn for fn in os.listdir(directory) if fn.endswith(".png")]
+  random.shuffle(filenames)
+  image0 = filenames[0]
+  #image1 = filenames[1]
+  #images = filenames[2:]
+  return render_to_response("tinder/home.html", locals(), RequestContext(request))
